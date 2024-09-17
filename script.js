a8601a1fc24cad34293a72c5f61ffccaab367a2d
sofia_liggio <sofialiggio994@gmail.com>
2024-09-17 19:12:31 +0200

    add input field and title

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
+// const btnPlus = document.getElementById("plus");
+// const btnMinus = document.querySelector("#minus");
+// const btnNumber = document.getElementById("btn");
 
+// btnPlus.addEventListener("click", function () {
+//   btnNumber.textContent++;
+// });
+
+// btnMinus.addEventListener("click", function () {
+//   btnNumber.textContent--;
+// });
+
+const listToWrite = document.getElementById("ordered-list");
+const inputField = document.getElementById("input-field");
+const btnEnter = document.getElementById("btn-star");
+const tasks = [];
+
+btnEnter.addEventListener("click", function () {
+  if (inputField.value === "") {
+    alert("Any things to do for today! 😜");
+  } else {
+    const task = inputField.value;
+    tasks.push(task);
+
+    const li = document.createElement("li");
+    li.textContent = task;
+    listToWrite.appendChild(li);
+
+    inputField.textContent = "";
+  }
+});
